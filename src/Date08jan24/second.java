af571311c61dcf9517cbe469978c59dbb09e437d
Routhu <chandraraorouthu9010@gmail.com>
2024-02-08 01:15:21 +0530

    commi

---
+++ src/Date08jan24/second.java
@@ -0,0 +1,15 @@
+package src.Date08jan24;
+
+import java.util.Scanner;
+
+public class second {
+    public static void main(String[] args) {
+        System.out.println("Enter the number");
+
+        Scanner s1 = new Scanner(System.in);
+
+        int a = s1.nextInt();
+
+        System.out.println(a);
+    }
+}
