9cbd4a72d4c717a960b085807bdcf2003ed43932
Routhu <chandraraorouthu9010@gmail.com>
2024-02-08 01:28:22 +0530

    tables

--- src/Date08jan24/Table.java
+++ src/Date08jan24/Table.java
@@ -1,2 +1,18 @@
-package src.Date08jan24;public class Table {
+package src.Date08jan24;
+
+import java.util.Scanner;
+
+public class Table {
+    public static void main(String[] args) {
+
+        System.out.println("Enter the table number");
+
+        Scanner s1=new Scanner(System.in);
+        int a = s1.nextInt();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            System.out.println(a + " X " + i + " = " + i*a);
+        }
+    }
 }
