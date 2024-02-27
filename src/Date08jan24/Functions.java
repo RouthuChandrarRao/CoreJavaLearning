4ea55305d4de874786146d7bafd7399852f12ebc
RouthuChandraRao <chandrarorouthu9010@gmail.com>
2024-02-28 02:36:30 +0530

    reverse

---
+++ src/Date08jan24/Functions.java
@@ -0,0 +1,31 @@
+package src.Date08jan24;
+
+
+import java.util.Scanner;
+
+public class Functions {
+
+
+    static void add( int a)
+    {
+        System.out.println("hero");
+    }
+
+     void add( int b, int c)
+    {
+        System.out.println("heros");
+    }
+
+    public static void main(String[] args) {
+
+      add(1);
+
+      Functions f1 = new Functions();
+
+      f1.add(2,3);
+
+
+
+
+    }
+}
